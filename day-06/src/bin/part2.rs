70bd49fa6246f54a0e53e1c5ed70f361c931f234
Mauricio Polvora <mauricio.polvora@gmail.com>
2023-12-08 19:55:20 +0000

    formating

--- day-06/src/bin/part2.rs
+++ day-06/src/bin/part2.rs
@@ -1,13 +1,21 @@
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("src/input1.txt")
-        .expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string("src/input1.txt").expect("Should have been able to read the file");
 
     let lines: Vec<String> = contents.lines().map(|line| line.replace(" ", "")).collect();
-    let time: Vec<u64> = lines[0].split(":").skip(1).map(|s| s.parse().unwrap()).collect();
-    let distance: Vec<u64> = lines[1].split(":").skip(1).map(|s| s.parse().unwrap()).collect();
+    let time: Vec<u64> = lines[0]
+        .split(":")
+        .skip(1)
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let distance: Vec<u64> = lines[1]
+        .split(":")
+        .skip(1)
+        .map(|s| s.parse().unwrap())
+        .collect();
 
     let mut map: HashMap<u64, u64> = HashMap::new();
 
@@ -22,19 +30,17 @@ fn main() {
     let mut total_wins: Vec<u64> = Vec::new();
 
     for (_time, wins) in possible_wins.iter() {
-
         let mut win = 0;
 
         for _wins in wins.iter() {
             win = win + 1;
-        }  
-        total_wins.push(win);  
+        }
+        total_wins.push(win);
     }
 
     let total_wins_product: u64 = total_wins.iter().fold(1, |product, &value| product * value);
 
     println!("Total wins product: {:?}", total_wins_product);
-
 }
 
 fn get_distance(time: u64, speed: u64) -> u64 {
@@ -42,21 +48,20 @@ fn get_distance(time: u64, speed: u64) -> u64 {
 }
 
 fn process_map(map: HashMap<u64, u64>) -> HashMap<u64, Vec<u64>> {
-
-    let mut possible_wins:HashMap<u64, Vec<u64>> = HashMap::new();
+    let mut possible_wins: HashMap<u64, Vec<u64>> = HashMap::new();
 
     for (time, distance) in map.iter() {
-
         let total_time = *time;
-
 
         for speed in 1..total_time {
             let try_distance = get_distance(total_time - speed, speed);
             if try_distance > *distance {
-                possible_wins.entry(total_time).or_insert(Vec::new()).push(try_distance);
+                possible_wins
+                    .entry(total_time)
+                    .or_insert(Vec::new())
+                    .push(try_distance);
             }
         }
-   
     }
 
     possible_wins
---
+++ day-07/src/bin/part1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let contents =
+        fs::read_to_string("src/input1.txt").expect("Should have been able to read the file");
+
+    println!("Hello, world!");
+}
