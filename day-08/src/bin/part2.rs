614ff29002b208d33d6a92abbba906619a070211
Mauricio Polvora <mpolvora@pictet-technologies.com>
2023-12-13 17:23:56 +0000

    day 08 done

--- day-08/src/bin/part2.rs
+++ day-08/src/bin/part2.rs
@@ -9,15 +9,32 @@ fn main() {
     println!("Map: {:?}", map);
     println!("Instructions: {:?}", instructions);
 
-    let starting = "AAA".to_string();
-    let mut steps = 0;
-    let mut current = starting.clone();
+    let mut values = Vec::new();
+    
+    for (key, _) in &map {
+        if key.ends_with('A') {
+            values.push(key.clone());
+        }
+    }
 
-    while current != "ZZZ" {
+    println!("Values: {:?}", values);
+
+    let mut steps = 0;
+    let mut current: Vec<String> = values.clone();
+
+    let mut keep = true;
+    while keep {
         let (new_current, new_steps) = process_instructions(&map, &instructions, current, steps);
         current = new_current;
         steps = new_steps;
+        if current.iter().all(|node| node.contains("Z")) {
+            println!("Found ZZZ in {} steps", steps);
+            keep = false;
+        }
     }
+
+
+
 }
 
 fn get_instructions(line: &str) -> Vec<char> {
@@ -30,26 +47,26 @@ fn get_instructions(line: &str) -> Vec<char> {
     instructions
 }
 
-fn process_instructions(map: &HashMap<String, (String, String)>, instructions: &[char], starting: String, steps: u32) -> (String, u32) {
+fn process_instructions(map: &HashMap<String, (String, String)>, instructions: &[char], starting: Vec<String>, steps: u32) -> (Vec<String>, u32) {
     let mut current = starting;
     for &instruction in instructions {
-        match instruction {
-            'R' => {
-                let (_value1, value2) = map.get(&current).unwrap();
-                current = value2.to_string();
+        current = current.into_iter().filter_map(|node| {
+            match instruction {
+                'R' => {
+                    let (_value1, value2) = map.get(&node).unwrap();
+                    Some(value2.to_string())
+                }
+                'L' => {
+                    let (value1, _value2) = map.get(&node).unwrap();
+                    Some(value1.to_string())
+                }
+                _ => {
+                    None
+                }
             }
-            'L' => {
-                let (value1, _value2) = map.get(&current).unwrap();
-                current = value1.to_string();
-            }
-            _ => {
-                break;
-            }
-        }
-        if current == "ZZZ" {
-            println!("Found ZZZ in {} steps", steps + instructions.len() as u32);
-            break;
-        }
+        }).collect();
+
+        println!("Current: {:?}", current);
     }
     (current, steps + instructions.len() as u32)
 }
